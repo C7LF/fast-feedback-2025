fc7c4e42ec6b2c6f607969a5c1a63180f5cc9b6e
C7LF <ccal72@yahoo.co.uk>
2020-11-30 21:00:38 +0000

    Authenication with firebase and github

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { AuthProvider } from '../lib/auth'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps}></Component>
+    </AuthProvider>
+  )
 }
 
 export default MyApp
